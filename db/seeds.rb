9be20f837e8c28c1b66a97c52f9324779395f7e9
Adit Mehta <aditmehta10@gmail.com>
2015-10-05 03:00:13 +0530

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ User.create!(name: "Example User",
   "foobar",
   password_confirmation: "foobar")
   admin: true)
+  activated: true,
+  activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
@@ -13,4 +15,6 @@ User.create!(name: "Example User",
               email: email,
               password: password,
               password_confirmation: password)
+              activated: true,
+              activated_at: Time.zone.now)
 end
